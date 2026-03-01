cbd20c83b0f49d1ccfb7808a7ec65410d505e17a
Umeshh27 <23MH1A05E7@acoe.edu.in>
2026-03-01 10:14:20 +0530

    1st commit

---
+++ src/main/java/com/example/demo/CseController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CseController {
+	@GetMapping("/cseadd")
+	public int addCSE(@RequestParam int a, @RequestParam int b) {
+		return a + b;
+	}
+	@GetMapping("/Roll")
+	public String roll() {
+		return "23MH1A05E7";
+	}
+}
